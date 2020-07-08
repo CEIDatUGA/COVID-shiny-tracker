c6b696633e6a4fc57209e41bfbf0a0b5366e6634
William Norfolk <wnorfolk4477@gmail.com>
2020-07-08 16:01:45 -0400

    small

--- app_new_working.R
+++ app_new_working.R
@@ -9,8 +9,7 @@ library(shinyWidgets)
 library(ggplot2)
 library(plotly)
 library(tm)
-#library(colorspace)
-library(randomcoloR)
+library(colorspace)
 
 
 #################################
@@ -539,9 +538,10 @@ server <- function(input, output, session) {
     }
     
     #set colors before filtering out locations so they remain the same
-    set.seed(1379)#keep the same color scheme
+    set.seed(1234)#keep the colors the same
     n_colors <- length(unique(plot_dat$location))
-    static_colors <- distinctColorPalette(n_colors)
+    add_palette <- colorRampPalette(c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666"))
+    static_colors <- add_palette(n_colors) %>% sample()
     plotcolors <<- stats::setNames(
       object = static_colors,
       #object = colorspace::rainbow_hcl(n_colors,start = 0, end = 360*(n_colors-1)/n_colors, alpha = 1, fixup = TRUE), 
